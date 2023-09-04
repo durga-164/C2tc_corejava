eb2a7e35635e6a737938a4fc9223f5a5b48d11a3
Durga R <137531455+durga-164@users.noreply.github.com>
2023-09-04 22:22:57 +0530

    firstcommit

--- corejava/src/com/tns/inheritance/Student.java
+++ corejava/src/com/tns/inheritance/Student.java
@@ -1,12 +1,41 @@
 package com.tns.inheritance;
 
 public class Student extends Citizen {
-private int rollNo;
- private String collageName;
-  
- public Student()
- {
-	 super();
- }
- public Student(String name,String aadhaarno,Stringw)
+    private int rollNo;
+    private String collageName;
+
+    public Student() {
+        // No need to call super() here, as the superclass already has a default constructor
+    }
+
+    public Student(String name, long aadhaarno, String address, long phone) {
+        super(name, aadhaarno, address, phone);
+    }
+
+    public Student(String name, long aadhaarno, String address, long phone, int rollNo, String collageName) {
+        super(name, aadhaarno, address, phone);
+        this.rollNo = rollNo;
+        this.collageName = collageName;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public void setCollageName(String collageName) {
+        this.collageName = collageName;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public String getCollageName() {
+        return collageName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [rollNo=" + rollNo + ", collageName=" + collageName + ", aadhaarno=" + getAadhaarno() + ", address=" + getAddress() + ", phone=" + getPhone() + "]";
+    }
 }
