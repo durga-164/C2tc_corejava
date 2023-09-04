9dcee6b75efaf139615833eccd10cc23ffb0f6c0
Durga R <137531455+durga-164@users.noreply.github.com>
2023-09-04 11:03:43 +0530

    firstcommit

--- corejava/src/com/tns/staticprogram/Myclassmain.java
+++ corejava/src/com/tns/staticprogram/Myclassmain.java
@@ -3,8 +3,14 @@ package com.tns.staticprogram;
 public class Myclassmain {
 	public static void main(String[]args)
 	{
-	Myclass.display();
+	//Myclass.display();
   Myclass ob= new Myclass();
  System.out.println(ob);
+ Myclass.display();
+ 
+ Myclass ob1= new Myclass();
+ System.out.println(ob1);
+ Myclass.display();
+
 }
 }
