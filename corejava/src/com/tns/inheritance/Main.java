c7d4312822ac1782fd17469144ddbe4acc1bbb2b
Durga R <137531455+durga-164@users.noreply.github.com>
2023-09-04 22:06:01 +0530

    firstcommit

--- corejava/src/com/tns/inheritance/Main.java
+++ corejava/src/com/tns/inheritance/Main.java
@@ -3,11 +3,11 @@ package com.tns.inheritance;
 public class Main {
 public static void main(String[]args)
 {
-   Lion obj=new Lion();
-   obj.wild();
-   obj.pet();
-   obj.lion();
-   obj.wolf();
-   obj.lionandcub();
+   Lion obj=new Lion();//an object called obj is created for the last child class Lion
+   obj.wild();//Animal class method is called
+   obj.pet();//Animal class method is called
+   obj.lion();//Wildanimal class method is called
+   obj.wolf();//Wildanimal class method is called
+   obj.lionandcub();//Lion class method is called
 }
 }
