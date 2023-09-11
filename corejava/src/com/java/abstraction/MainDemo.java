36b5a2e447205d379d71fa16ba3a41784d9c7f29
Durga R <137531455+durga-164@users.noreply.github.com>
2023-09-11 06:28:58 +0530

    interface example

---
+++ corejava/src/com/java/abstraction/MainDemo.java
@@ -0,0 +1,13 @@
+package com.java.abstraction;
+
+public class MainDemo {
+public static void main(String[]args)
+{
+	Square sq=new Square();
+	Rectangle r=new Rectangle();
+	System.out.println(sq);
+	System.out.println(r);
+	sq.calculateArea();
+	r.calculateArea();
+}
+}
---
+++ corejava/src/com/tns/finalvariable/Finalchildclass.java
@@ -0,0 +1,8 @@
+package com.tns.finalvariable;
+final class Finalclass{
+	
+}
+//final class cannot be inherited
+public class Finalchildclass extends Finalclass {
+
+}
